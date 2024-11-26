eef3d9d910196a818406568f5e16a5fdae46d8f0
guipereirar <gpereira383@gmail.com>
2024-11-26 15:38:29 -0300

    feat: added food and restaurants

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,11 @@ import { Header } from "../components/header";
 
 import Constants from "expo-constants";
 import { Banner } from "../components/banner";
+import { Search } from "../components/search";
+import { Section } from "../components/section";
+import { TrendingFoods } from "../components/trending";
+import { Restaurants } from "../components/restaurants";
+import { RestaurantsVerticalList } from "../components/list";
 
 const statusBarHeight = Constants.statusBarHeight;
 
@@ -17,7 +22,36 @@ export default function Index() {
         <Header />
 
         <Banner />
+
+        <Search />
       </View>
+
+      <Section
+        name="Comidas em alta"
+        label="Veja mais"
+        action={() => console.log("CLCOU NO VEJA MAIS")}
+        size="text-xl"
+      />
+
+      <TrendingFoods />
+
+      <Section
+        name="Famosos no DEVFOOD"
+        label="Veja todos"
+        action={() => console.log("CLCOU NO VEJA TODOS")}
+        size="text-xl"
+      />
+
+      <Restaurants />
+
+      <Section
+        name="Restaurantes"
+        label="Veja todos"
+        action={() => console.log("CLCOU NO RESTAURANTES")}
+        size="text-xl"
+      />
+
+      <RestaurantsVerticalList />
     </ScrollView>
   );
 }
---
+++ src/components/list/index.tsx
@@ -0,0 +1,29 @@
+import { View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import { RestaurantItem } from "./item";
+
+export interface RestaurantProps {
+  id: string;
+  name: string;
+  image: string;
+}
+
+export function RestaurantsVerticalList() {
+  const [restaurants, setRestaurants] = useState<RestaurantProps[]>([]);
+  useEffect(() => {
+    async function getRestaurants() {
+      const response = await fetch("http://192.168.0.71:3000/restaurants");
+      const data = await response.json();
+      setRestaurants(data);
+    }
+
+    getRestaurants();
+  }, []);
+  return (
+    <View className="px-4 flex-1 w-full h-full mg-11 gap-4">
+      {restaurants.map((item) => (
+        <RestaurantItem item={item} key={item.id} />
+      ))}
+    </View>
+  );
+}
